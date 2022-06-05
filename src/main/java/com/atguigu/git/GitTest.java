00cc881467a3562bfe15d81abe107f94406cda5c
yangy <2487755249@qq.com>
2022-06-05 17:21:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author yangy
+ * @create 2022-06-05 16:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("two commit");
+    }
+}
---
+++ src/main/java/com/atguigu/git/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yangy
+ * @DATE 2022-06-05 17:01
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
