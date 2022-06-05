17da4849dd421c2fb06dbd4fdea0237fb8ed60fc
yangy <2487755249@qq.com>
2022-06-05 17:37:55 +0800

    two commit

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -6,6 +6,10 @@ package com.atguigu.git;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello  world");
+        System.out.println("hello  world");
+        System.out.println("hello  world");
+        System.out.println("hello  world");
+
     }
 }
