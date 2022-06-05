381a07b59433ad7b48ba93886842706f1e377dc8
yangy <2487755249@qq.com>
2022-06-05 18:05:34 +0800

    master commit

--- src/main/java/com/atguigu/git/Test.java
+++ src/main/java/com/atguigu/git/Test.java
@@ -9,14 +9,8 @@ import javax.sound.midi.Soundbank;
  */
 public class Test {
     public static void main(String[] args) {
-
-
         System.out.println("git 11111111111");
         System.out.println("git 22222222222");
         System.out.println("git 33333333333");
-
     }
-
-
-
 }
